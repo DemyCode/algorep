93ac2e09eb45b6426d8744353b0e44e1d32f9a75
Axen Georget <mail@axen.io>
2020-11-20 22:43:30 +0100

    feat: add prob to the receive message

--- src/rpc/lib-rpc.hh
+++ src/rpc/lib-rpc.hh
@@ -6,4 +6,4 @@
 #include "rpc.hh"
 
 void send_message(const RPC& rpc_message, int destination, MPI_Request& request, int tag = 0);
-RPCQuery receive_message(int source, int tag = 0);
+std::shared_ptr<RPCQuery> receive_message(int source, int tag = 0);
