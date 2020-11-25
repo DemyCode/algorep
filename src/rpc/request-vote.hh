bee18db0e1ca35b69c8b9589da5abda1477c2d4e
Mickael Gandon <mickael.gandon@epita.fr>
2020-11-25 23:14:46 +0100

    [REQUESTVOTE] Documentation

--- src/rpc/request-vote.hh
+++ src/rpc/request-vote.hh
@@ -8,29 +8,45 @@
 class RequestVote : public RPC
 {
 public:
+    ///Constructor
     RequestVote(int term, size_t candidate_id, size_t last_log_index, int last_log_term);
+    ///Constructor
     RequestVote(int term, const json& serialized);
+    ///Constructor
     RequestVote(int term, const std::string& serialized);
 
+    /**
+     * @brief Serialize the message
+     * 
+     * @return json 
+     */
     json serialize_message() const override;
 
-    // candidate requesting vote
+    /// candidate requesting vote
     const size_t candidate_id_;
-    // index of candidate's last log entry
+    /// index of candidate's last log entry
     const int last_log_index_;
-    // term of candidate's last log entry
+    /// term of candidate's last log entry
     const int last_log_term_;
 };
 
 class RequestVoteResponse : public RPC
 {
 public:
+    ///Constructor
     RequestVoteResponse(int term, bool vote_granted_);
+    ///Constructor
     RequestVoteResponse(int term, const json& serialized_json);
+    ///Constructor
     RequestVoteResponse(int term, const std::string& serialized);
 
+    /**
+     * @brief Serialize the message
+     * 
+     * @return json 
+     */
     json serialize_message() const override;
 
-    // true means candidate received vote
+    /// true means candidate received vote
     const bool vote_granted_;
 };
