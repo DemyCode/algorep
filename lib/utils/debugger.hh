2f7c950bf3927908a7c3a025ee6c79d51e7b5814
Mehdi Bekhtaoui <mehdi.bekhtaoui@epita.fr>
2020-11-25 23:24:25 +0100

    [DOC] debug write doc

--- lib/utils/debugger.hh
+++ lib/utils/debugger.hh
@@ -6,4 +6,8 @@
 
 #include "mpi_classes/process-information.hh"
 
+/**
+ * @brief Function to write in a debug file, only works in Debug mode
+ * @param debug_message
+ */
 void debug_write(const std::string& debug_message);
