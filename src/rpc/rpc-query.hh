403ce17bcabd529d0feb6e5e6a4aefbf62c68582
Mickael Gandon <mickael.gandon@epita.fr>
2020-11-25 23:21:54 +0100

    [Rpc-Query] Docu

--- src/rpc/rpc-query.hh
+++ src/rpc/rpc-query.hh
@@ -26,10 +26,18 @@ public:
                                    GetState,
                                    GetStateResponse>;
 
+    ///Constructor
     RPCQuery(size_t source_rank, RPC::RPC_TYPE type, int term, content_t content);
 
+    ///Source rank
     const size_t source_rank_;
+
+    ///Type of the RPC
     const RPC::RPC_TYPE type_;
+
+    ///Term of the RPC
     const int term_;
+
+    ///Content of the RPC
     const content_t content_;
 };
